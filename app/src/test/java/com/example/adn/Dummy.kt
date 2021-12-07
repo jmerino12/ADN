7dd79826f6b45601c8a69c4a02b6caf5edf58569
jonathan.merino <jonathan.merino@ceiba.com.co>
2021-12-07 09:05:33 -0500

    test

---
+++ app/src/test/java/com/example/adn/Dummy.kt
@@ -0,0 +1,11 @@
+package com.example.adn
+
+import org.junit.Assert
+import org.junit.Test
+
+class Dummy {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
