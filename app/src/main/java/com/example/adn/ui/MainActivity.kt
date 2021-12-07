cc55005e8c9fa4d9abf3898fe9f09892c4bde40c
jonathan.merino <jonathan.merino@ceiba.com.co>
2021-12-07 08:59:45 -0500

    removiendo los dummytest

--- app/src/main/java/com/example/adn/ui/MainActivity.kt
+++ app/src/main/java/com/example/adn/ui/MainActivity.kt
@@ -3,26 +3,16 @@ package com.example.adn.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.adn.databinding.ActivityMainBinding
-import com.example.domain.entities.Vehicle
+
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var listVehicles: MutableList<Vehicle>
-
-
-    private var rButtonCar: Boolean? = null
-    private var rButtonMotorcycle: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        listVehicles = ArrayList()
-
-        binding.addVehicle.setOnClickListener {
-
-        }
 
     }
 
