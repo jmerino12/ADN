8bd911e5080f9e73adf117f2f2a3a71230767c50
jonathan.merino <jonathan.merino@ceiba.com.co>
2021-12-07 08:49:28 -0500

    sonar, capa de dominio

--- app/src/main/java/com/example/adn/model/database/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.adn.model.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Car::class, Motorcycle::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun vehicleDao(): VehicleDao
-}
